22f1b1dddf09c5c0d32283c2dc12156b29425237
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-02-28 12:17:25 -0500

    handleSubmit adding transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
+
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount: +amount
+    };
+
+    addTransaction(newTransaction);
+  };
 
   return (
     <Fragment>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
           <input
