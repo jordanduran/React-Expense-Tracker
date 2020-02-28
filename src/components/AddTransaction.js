819822662523dc04aee17bc2f98b3a257449d605
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-02-28 11:11:09 -0500

    useState hooks for text and amount

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <Fragment>
       <h3>Add new transaction</h3>
       <form>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
-          <input type='text' name='text' placeholder='Enter text...' />
+          <input
+            type='text'
+            name='text'
+            value={text}
+            onChange={e => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
         </div>
         <div className='form-control'>
           <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' name='amount' placeholder='Enter amount...' />
+          <input
+            type='number'
+            name='amount'
+            placeholder='Enter amount...'
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+          />
         </div>
         <button className='btn'>Add transaction</button>
       </form>
